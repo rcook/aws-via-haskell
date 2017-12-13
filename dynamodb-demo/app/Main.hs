a03dc9eb8d8e56459a479a84f5bcd95956689eca
Richard Cook <rcook@rcook.org>
2017-12-13 08:22:43 -0800

    Combine database info into record

--- dynamodb-demo/app/Main.hs
+++ dynamodb-demo/app/Main.hs
@@ -2,10 +2,15 @@
 -- Copyright (C) 2017, All rights reserved.
 --------------------------------------------------
 
+-- All of amazonka APIs use Data.Text.Text by default which is nice
 {-# LANGUAGE OverloadedStrings #-}
+
+-- Allows record fields to expanded automatically
+{-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
+-- All imports are explicit so we can see exactly where each function comes from
 import           Control.Exception.Lens (handling)
 import           Control.Lens ((<&>), (^.), (.~), (&), set)
 import           Control.Monad (void, when)
@@ -47,11 +52,12 @@ import           System.IO (stdout)
 say :: MonadIO m => Text -> m ()
 say = liftIO . Text.putStrLn
 
-defaultRegion :: Region
-defaultRegion = Ohio
-
-tableName :: Text
-tableName = "BAR"
+data DynamoDBEnv = DynamoDBEnv
+    { env :: Env
+    , service :: Service
+    , region :: Region
+    , tableName :: Text
+    }
 
 -- Creates a table in DynamoDB and waits until table is in active state
 -- Demonstrates:
@@ -60,10 +66,10 @@ tableName = "BAR"
 -- * How to handle exceptions in lenses
 -- * Basic use of amazonka-style lenses
 -- * How to wait on an asynchronous operation
-doCreateTableIfNotExists :: Env -> Service -> IO ()
-doCreateTableIfNotExists env db = do
-    runResourceT . runAWST env . within defaultRegion $ do
-        reconfigure db $ do
+doCreateTableIfNotExists :: DynamoDBEnv -> IO ()
+doCreateTableIfNotExists DynamoDBEnv{..} = do
+    runResourceT . runAWST env . within region $ do
+        reconfigure service $ do
             exists <- handling _ResourceInUseException (const (pure True)) $ do
                 void $ send $ createTable
                     tableName
@@ -74,34 +80,34 @@ doCreateTableIfNotExists env db = do
             when (not exists) (void $ await tableExists (describeTable tableName))
 
 -- Deletes a table in DynamoDB if it exists and waits until table no longer exists
-doDeleteTableIfExists :: Env -> Service -> IO ()
-doDeleteTableIfExists env db = do
-    runResourceT . runAWST env . within defaultRegion $ do
-        reconfigure db $ do
+doDeleteTableIfExists :: DynamoDBEnv -> IO ()
+doDeleteTableIfExists DynamoDBEnv{..} = do
+    runResourceT . runAWST env . within region $ do
+        reconfigure service $ do
             exists <- handling _ResourceNotFoundException (const (pure False)) $ do
                 void $ send $ deleteTable tableName
                 return True
             when exists (void $ await tableNotExists (describeTable tableName))
 
 -- Puts an item into the DynamoDB table
-doPutItem :: Env -> Service -> IO ()
-doPutItem env db = do
+doPutItem :: DynamoDBEnv -> IO ()
+doPutItem DynamoDBEnv{..} = do
     let item = HashMap.fromList
             [ ("counter_name", attributeValue & avS .~ Just "foo")
             , ("counter_value", attributeValue & avN .~ Just "1001")
             ]
-    runResourceT . runAWST env . within defaultRegion $ do
-        reconfigure db $ do
+    runResourceT . runAWST env . within region $ do
+        reconfigure service $ do
             void $ send $ putItem tableName & piItem .~ item
 
 -- Gets an item from the DynamoDB table
-doGetItem :: Env -> Service -> IO ()
-doGetItem env db = do
+doGetItem :: DynamoDBEnv -> IO ()
+doGetItem DynamoDBEnv{..} = do
     let key = HashMap.fromList
             [ ("counter_name", attributeValue & avS .~ Just "foo")
             ]
-    runResourceT . runAWST env . within defaultRegion $ do
-        reconfigure db $ do
+    runResourceT . runAWST env . within region $ do
+        reconfigure service $ do
             result <- send $ getItem tableName & giKey .~ key
             void $ case HashMap.lookup "counter_value" (result ^. girsItem) of
                 Nothing -> say "No counter_value field"
@@ -120,20 +126,22 @@ main = do
         newEnv Discover <&> set envLogger logger
 
     -- Use this to run against a local DynamoDB instance
-    let db = setEndpoint False "localhost" 8000 dynamoDB
+    let service = setEndpoint False "localhost" 8000 dynamoDB
     -- Use this to run against a DynamoDB instance running on AWS
     --let db = dynamoDB
 
+    let dynamoDBEnv = DynamoDBEnv env service Ohio "BAR"
+
     putStrLn "DeleteTable"
-    doDeleteTableIfExists env db
+    doDeleteTableIfExists dynamoDBEnv
 
     putStrLn "CreateTable"
-    doCreateTableIfNotExists env db
+    doCreateTableIfNotExists dynamoDBEnv
 
     putStrLn "PutItem"
-    doPutItem env db
+    doPutItem dynamoDBEnv
 
     putStrLn "GetItem"
-    doGetItem env db
+    doGetItem dynamoDBEnv
 
     putStrLn "Done"
