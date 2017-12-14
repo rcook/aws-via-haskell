97a77ed09c8d286e0bc56150653eeb34eaad7c50
Richard Cook <rcook@rcook.org>
2017-12-14 09:57:55 -0800

    Strip out noise

--- dynamodb-demo/lib/DynamoDBDemo.hs
+++
@@ -1,8 +0,0 @@
---------------------------------------------------
--- Copyright (C) 2017, All rights reserved.
---------------------------------------------------
-
-module DynamoDBDemo (sample) where
-
-sample :: IO ()
-sample = putStrLn "Hello from DynamoDBDemo.sample"
