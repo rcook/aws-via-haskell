f2f37f1a908ddc34f0e84d656ead89be1ea28393
Richard Cook <rcook@rcook.org>
2017-12-15 15:42:36 -0800

    Create empty s3-demo project

---
+++ s3-demo/Main.hs
@@ -0,0 +1,8 @@
+--------------------------------------------------
+-- Copyright (C) 2017, All rights reserved.
+--------------------------------------------------
+
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "Hello from S3Demo.main"
---
+++ s3-demo/Setup.hs
@@ -0,0 +1,10 @@
+--------------------------------------------------
+-- Copyright (C) 2017, All rights reserved.
+--------------------------------------------------
+
+module Main (main) where
+
+import           Distribution.Simple
+
+main :: IO ()
+main = defaultMain
